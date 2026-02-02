24536fd63b48ef8f4439f9b18daebbacab38ca67
Juan Lin <juanlin@gmail.com>
2026-02-03 03:41:23 +0800

    Make Window Navigation descriptions consistent (Go to...)

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,7 +2,7 @@ vim.g.mapleader = ' '
 vim.keymap.set('n', '<Esc>', '<Cmd>nohlsearch<CR>')
 
 -- Window navigation
-vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Focus on left window' })
-vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Focus on below window' })
-vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Focus on above window' })
-vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Focus on right window' })
+vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Go to the left window' })
+vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Go to the down window' })
+vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Go to the up window' })
+vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Go to the right window' })
