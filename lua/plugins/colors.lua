768fe560132884c0b21a69a46e7b95d235997577
Juan Lin <juanlin@gmail.com>
2026-02-03 01:33:26 +0800

    Colors: Install and use Catppuccin theme

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,15 +1,31 @@
 return {
-  'folke/tokyonight.nvim',
-  lazy = false, -- load during startup as my main colorscheme
-  priority = 1000, -- load before all other start plugins
-  opts = {
-    style = 'moon', -- day, night, moon, storm
-    transparent = true,
-    -- lualine_bold = true,
-    -- dim_inactive = true,
+  {
+    'folke/tokyonight.nvim',
+    lazy = false, -- load during startup as my main colorscheme
+    priority = 1000, -- load before all other start plugins
+    opts = {
+      style = 'moon', -- day, night, moon, storm
+      transparent = true,
+      -- lualine_bold = true,
+      -- dim_inactive = true,
+    },
+    -- config = function(_, opts)
+    --   require('tokyonight').setup(opts)
+    --   vim.cmd.colorscheme 'tokyonight'
+    -- end
   },
-  config = function(_, opts)
-    require('tokyonight').setup(opts)
-    vim.cmd.colorscheme 'tokyonight'
-  end
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    opts = {
+      flavour = 'macchiato',  -- latte, frappe, macchiato, mocha
+      -- transparent_background = true,
+      auto_integrations = true,
+    },
+    config = function(_, opts)
+      require('catppuccin').setup(opts)
+      vim.cmd.colorscheme 'catppuccin'
+    end
+  },
 }
