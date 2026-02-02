f948ae27a95632668e4a7aaa02fd401e581edc49
Juan Lin <juanlin@gmail.com>
2026-02-03 06:48:29 +0800

    Fix <leader>w proxy to be able to close window with <C-w>c

--- lua/config/which-key.lua
+++ lua/config/which-key.lua
@@ -4,6 +4,10 @@ wk.add({
   { '<leader>ff', '<Cmd>Telescope find_files<CR>', desc = 'Find File' },
   { '<leader>fg', '<Cmd>Telescope live_grep<CR>', desc = 'Live Grep' },
   { '<leader>fb', '<Cmd>Telescope buffers<CR>', desc = 'Buffers' },
-  { '<leader>w', proxy = '<C-w>', group = 'Windows' },
-  { '<leader>e', '<Cmd>Ex<CR>', desc = 'File Explorer'} --, hidden = true },
+
+  { '<leader>e', '<Cmd>Ex<CR>', desc = 'File Explorer'},
+  { '<leader>w', proxy = '<C-w>', group = 'windows' },
+
+  { '<C-w>', group = 'windows' }, -- match proxy group name (consistency)
+  { '<C-w>c', desc = 'Close window' }, -- explicit set to work in proxy
 })
