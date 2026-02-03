0e932c6966a704d9439607d11bb59a69d143afcf
Juan Lin <juanlin@gmail.com>
2026-02-03 09:29:31 +0800

    Move leader key mappings back to keymaps
    which-key's proxy is buggy, e.g.: <C-w>< and <C-w>c don't work.

--- lua/config/which-key.lua
+++ lua/config/which-key.lua
@@ -4,10 +4,4 @@ wk.add({
   { '<leader>ff', '<Cmd>Telescope find_files<CR>', desc = 'Find File' },
   { '<leader>fg', '<Cmd>Telescope live_grep<CR>', desc = 'Live Grep' },
   { '<leader>fb', '<Cmd>Telescope buffers<CR>', desc = 'Buffers' },
-
-  { '<leader>e', '<Cmd>Ex<CR>', desc = 'File Explorer'},
-  { '<leader>w', proxy = '<C-w>', group = 'windows' },
-
-  { '<C-w>', group = 'windows' }, -- match proxy group name (consistency)
-  { '<C-w>c', desc = 'Close window' }, -- explicit set to work in proxy
 })
