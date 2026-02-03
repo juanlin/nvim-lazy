a2072e951320a8623418eb08dedb8ad2d7306688
Juan Lin <juanlin@gmail.com>
2026-02-03 10:35:36 +0800

    Configure <leader>w to work with -c, -<, -[count]X
    1. Bind <leader>w to <C-w> in keymaps.
    
    2. Set <leader>w as proxy to <C-w> to enable hints for <leader>w
       * No popup appears for <leader>w (only ^W awaiting key).
       * Works for <leader>... (popup)... then w.

--- lua/config/which-key.lua
+++ lua/config/which-key.lua
@@ -4,4 +4,5 @@ wk.add({
   { '<leader>ff', '<Cmd>Telescope find_files<CR>', desc = 'Find File' },
   { '<leader>fg', '<Cmd>Telescope live_grep<CR>', desc = 'Live Grep' },
   { '<leader>fb', '<Cmd>Telescope buffers<CR>', desc = 'Buffers' },
+  { '<leader>w', proxy = '<C-w>' }, -- show <C-w> popup when <leader>w
 })
