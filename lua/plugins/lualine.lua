514bb4c60eb3e8c7ef1584002ca5d7d271597a1b
Juan Lin <juanlin@gmail.com>
2026-02-05 03:23:47 +0800

    chore: disable lualine separators

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,6 +1,12 @@
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-mini/mini.icons' },
+  opts = {
+    options = {
+      section_separators = '',
+      component_separators = '',
+    },
+  },
   config = function(_, opts)
     require('lualine').setup(opts)
     vim.opt.showmode = false  -- hide mode notification
