02e3757e6ec31ae279abfcee62885125856c08c8
Juan Lin <juanlin@gmail.com>
2026-02-05 04:29:19 +0800

    style: shorten vim.keymap.set to 'map'

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,16 +1,18 @@
+local map = vim.keymap.set
+
 vim.g.mapleader = ' '
-vim.keymap.set('n', '<leader>e', '<Cmd>Ex<CR>', { desc = 'File Explorer' })
-vim.keymap.set('n', '<leader>w', '<C-w>', { desc = 'window', })
+map('n', '<leader>e', '<Cmd>Ex<CR>', { desc = 'File Explorer' })
+map('n', '<leader>w', '<C-w>', { desc = 'window', })
 
 -- Telescope
-vim.keymap.set('n', '<leader>ff', '<Cmd>Telescope find_files<CR>', { desc = 'Find File' })
-vim.keymap.set('n', '<leader>fg', '<Cmd>Telescope live_grep<CR>', { desc = 'Live Grep' })
-vim.keymap.set('n', '<leader>fb', '<Cmd>Telescope buffers<CR>', { desc = 'Buffers' })
+map('n', '<leader>ff', '<Cmd>Telescope find_files<CR>', { desc = 'Find File' })
+map('n', '<leader>fg', '<Cmd>Telescope live_grep<CR>', { desc = 'Live Grep' })
+map('n', '<leader>fb', '<Cmd>Telescope buffers<CR>', { desc = 'Buffers' })
 
-vim.keymap.set('n', '<Esc>', '<Cmd>nohlsearch<CR>')
+map('n', '<Esc>', '<Cmd>nohlsearch<CR>')
 
 -- Window navigation
-vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Go to the left window' })
-vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Go to the down window' })
-vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Go to the up window' })
-vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Go to the right window' })
+map('n', '<C-h>', '<C-w>h', { desc = 'Go to the left window' })
+map('n', '<C-j>', '<C-w>j', { desc = 'Go to the down window' })
+map('n', '<C-k>', '<C-w>k', { desc = 'Go to the up window' })
+map('n', '<C-l>', '<C-w>l', { desc = 'Go to the right window' })
