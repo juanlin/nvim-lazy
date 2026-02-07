b175cb3bdd30ccf1e7dc28dc06c5a52108797094
Juan Lin <juanlin@gmail.com>
2026-02-07 16:07:08 +0800

    perf: lazy load mini.icons (lazy=true)

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -2,6 +2,7 @@ return {
   {
     'nvim-mini/mini.icons',
     version = false,  -- install from main branch
+    lazy = true,
     config = function(_, opts)
       require('mini.icons').setup(opts)
       MiniIcons.mock_nvim_web_devicons()
