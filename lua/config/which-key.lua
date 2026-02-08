180591d2cb5f80fa3f96fffacfcb4efd1256b1f6
Juan Lin <juanlin@gmail.com>
2026-02-08 14:15:51 +0800

    chore(wk): clarify keymaps and whichkey

--- lua/config/which-key.lua
+++ lua/config/which-key.lua
@@ -1,12 +1,13 @@
 local wk = require('which-key')
 wk.add({
+  -- name group keys
   { '<leader>f', group = 'Telescope' },
-  { '<leader>w', proxy = '<C-w>' }, -- show <C-w> popup for <leader>w
+  -- register as group with more subsequent keys
+  { '<leader>w', proxy = '<C-w>' },
 })
 
+-- Handle <Leader>w registered as one key (when hit quickly)
 local function myfunc()
-  wk.show({ keys = '<C-w>', loop = false, delay = 250, })
+  wk.show({ keys = '<C-w>', loop = false, delay = 0, })
 end
-
--- Passing <C-w> directly will not trigger popup when hitting <leader>w
-vim.keymap.set('n', '<leader>w', myfunc, { desc = 'window', })
+vim.keymap.set('n', '<leader>w', myfunc, { desc = 'Window commands' })
