a051a9e633e21083c0aea33d0ae82417f1269082
Juan Lin <9691057+juanlin@users.noreply.github.com>
2026-02-09 07:48:50 +0800

    fix(wk): pull which-key off fork (fix proxy binds)

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,5 +1,5 @@
 return {
-  'folke/which-key.nvim',
+  'juanlin/which-key.nvim',
   opts = {
     delay = 250,
     spec = {
@@ -9,10 +9,6 @@ return {
 
       -- set as proxy to <C-w> (do not re-map to avoid issues)
       { '<leader>w', proxy = '<C-w>', group = 'Window commands' },
-      -- adding desc explicitly allows <leader>wc to properly capture!
-      { '<C-w>c', desc = 'Close window!' },
-      -- ... but does not work for '<'
-      { '<C-w><', desc = 'Decrease width!' },
     },
   },
 }
