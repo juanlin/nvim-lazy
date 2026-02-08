5194db9a3a6446cefc112b462dba02cad5a4c3f9
Juan Lin <juanlin@gmail.com>
2026-02-09 06:09:40 +0800

    fix(wk): capture <leader>wc without going into Ins

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -6,8 +6,13 @@ return {
       -- name group keys
       { '<leader>f', group = 'Telescope' },
       { 'gs', group = 'Surround', mode = { 'n', 'v' } },
+
       -- set as proxy to <C-w> (do not re-map to avoid issues)
       { '<leader>w', proxy = '<C-w>', group = 'Window commands' },
+      -- adding desc explicitly allows <leader>wc to properly capture!
+      { '<C-w>c', desc = 'Close window!' },
+      -- ... but does not work for '<'
+      { '<C-w><', desc = 'Decrease width!' },
     },
   },
 }
