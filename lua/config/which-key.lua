d31667e99f93eccb02f15682141f855739882ebb
Juan Lin <juanlin@gmail.com>
2026-02-08 23:39:25 +0800

    style(wk): move wk.add() mappings to plugin spec

--- lua/config/which-key.lua
+++ lua/config/which-key.lua
@@ -1,8 +1,8 @@
-local wk = require('which-key')
-wk.add({
-  -- name group keys
-  { '<leader>f', group = 'Telescope' },
-  { 'gs', group = 'Surround', mode = { 'n', 'v' } },
-  -- set as proxy to <C-w> (do not re-map to avoid issues)
-  { '<leader>w', proxy = '<C-w>', group = 'Window commands' },
-})
+-- local wk = require('which-key')
+-- wk.add({
+--   -- name group keys
+--   { '<leader>f', group = 'Telescope' },
+--   { 'gs', group = 'Surround', mode = { 'n', 'v' } },
+--   -- set as proxy to <C-w> (do not re-map to avoid issues)
+--   { '<leader>w', proxy = '<C-w>', group = 'Window commands' },
+-- })
--- lua/plugins/one-liners.lua
+++ lua/plugins/one-liners.lua
@@ -6,6 +6,16 @@ return {
   --   'brenoprata10/nvim-highlight-colors', opts = {}, event = 'VeryLazy',
   -- },
   { -- Show key hints
-    'folke/which-key.nvim', opts = { delay = 250 }
+    'folke/which-key.nvim',
+    opts = {
+      delay = 250,
+      spec = {
+        -- name group keys
+        { '<leader>f', group = 'Telescope' },
+        { 'gs', group = 'Surround', mode = { 'n', 'v' } },
+        -- set as proxy to <C-w> (do not re-map to avoid issues)
+        { '<leader>w', proxy = '<C-w>', group = 'Window commands' },
+      },
+    },
   },
 }
