148d6638e2b87e23fb65e1cce7accaceb0a6ab7c
Juan Lin <9691057+juanlin@users.noreply.github.com>
2026-02-09 19:49:50 +0800

    fix: change autocmd to BufWinEnter, shuffle if con

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -10,25 +10,24 @@ vim.api.nvim_create_autocmd('FileType', {
 })
 
 -- Return cursor to last known position
-vim.api.nvim_create_autocmd('BufReadPost', {
+vim.api.nvim_create_autocmd('BufWinEnter', {
   group = general_settings,
   pattern = '*',
   callback = function()
-    local mark = vim.api.nvim_buf_get_mark(0, '"')
-    local lcount = vim.api.nvim_buf_line_count(0)
     local ft = vim.bo.filetype
-    local current_line = vim.api.nvim_win_get_cursor(0)[1]
 
-    -- Check if line has been specified via 'nvim file +xx'
-    if current_line > 1 then
+    print(ft)
+    if vim.fn.line('.') > 1   -- specified via 'nvim file +xx'
+      or ft:find('commit')
+      or vim.tbl_contains({ 'xxd', 'gitrebase', 'tutor'}, ft)
+      or vim.wo.diff then
       return
     end
 
-    -- Check if mark is valid and skip specific filetypes/diff mode
-    if mark[1] > 0 and mark[1] <= lcount
-       and ft ~= 'commit'
-       and not vim.tbl_contains({ 'xxd', 'gitrebase', 'tutor' }, ft)
-       and not vim.wo.diff then
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+
+    if mark[1] > 0 and mark[1] <= lcount then
       vim.api.nvim_win_set_cursor(0, mark)
     end
   end,
