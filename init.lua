8a3fd03ceb88c6536ae273472c46e21dec18304e
Juan Lin <9691057+juanlin@users.noreply.github.com>
2026-02-10 21:24:42 +0800

    feat(lsp): added luals

--- init.lua
+++ init.lua
@@ -2,3 +2,6 @@ require('config.options')
 require('config.keymaps')
 require('config.autocmds')
 require('config.lazy')
+vim.lsp.enable({
+  'luals',
+})
---
+++ lsp/luals.lua
@@ -0,0 +1,16 @@
+return {
+  cmd = { 'lua-language-server' },
+  root_markers = { '.luarc.json', '.luarc.jsonc' },
+  filetypes = { 'lua' },
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        globals = { 'vim' }
+      },
+      signatureHelp = { enabled = true },
+    },
+  },
+}
