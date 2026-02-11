bcc8483914ceb72754843ced711857c7eab08280
Juan Lin <9691057+juanlin@users.noreply.github.com>
2026-02-11 08:48:13 +0800

    Revert "feat: remove whichkey"
    This reverts commit 11d7e17cabcb985f7f4abb7dd9989018938ba3b1.

--- lua/plugins/one-liners.lua
+++ lua/plugins/one-liners.lua
@@ -5,4 +5,7 @@ return {
   -- { -- Show CSS colors, setting opts calls setup
   --   'brenoprata10/nvim-highlight-colors', opts = {}, event = 'VeryLazy',
   -- },
+  { -- Show key hints
+    'folke/which-key.nvim', opts = { delay = 1000 }
+  },
 }
