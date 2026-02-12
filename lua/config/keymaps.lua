b7c0c0efc307f5e9d8523c5fbce8a46f84e6904b
Juan Lin <9691057+juanlin@users.noreply.github.com>
2026-02-12 12:57:36 +0800

    feat(maps): system clipboard and buffer navigation

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -14,5 +14,15 @@ map('n', '<C-j>', '<C-w>j', { desc = 'Focus down' })
 map('n', '<C-k>', '<C-w>k', { desc = 'Focus up' })
 map('n', '<C-l>', '<C-w>l', { desc = 'Focus right' })
 
+-- Easier interaction with the system clipboard
+map({ 'n', 'x' }, '<leader>y', '"+y', { desc = 'Copy to Clipboard' })
+map({ 'n', 'x' }, '<leader>p', '"+p', { desc = 'Paste Clipboard' })
+map({ 'n', 'x' }, '<leader>P', '"+P', { desc = 'Paste Clipboard before' })
+
+-- Navigating buffers
+map('n', '<leader>bb', '<C-^>', { desc = 'Switch to alternate buffer' })
+map('n', '<leader>bn', '<Cmd>bnext<CR>', { desc = 'Next buffer' })
+map('n', '<leader>bp', '<Cmd>bprevious<CR>', { desc = 'Previous buffer' })
+
 -- Other
 map('n', '<Esc>', '<Cmd>nohlsearch<CR>')
