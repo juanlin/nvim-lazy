d5d78eb1298f7c9d6bc9a1195d19c65e7167e90a
Juan Lin <9691057+juanlin@users.noreply.github.com>
2026-02-27 17:50:01 +0800

    feat(minifiles): create toggle for hidden files

--- lua/plugins/mini-files.lua
+++ lua/plugins/mini-files.lua
@@ -59,10 +59,27 @@ return {
       vim.keymap.set('n', lhs, rhs, { buffer = buf_id, desc = desc })
     end
 
+    -- Create toggle to show/hide hidden files
+    local show_dotfiles = true
+    local filter_show = function(fs_entry)
+      return true
+    end
+    local filter_hide = function(fs_entry)
+      return not vim.startswith(fs_entry.name, '.')
+    end
+    local toggle_dotfiles = function()
+      show_dotfiles = not show_dotfiles
+      local new_filter = show_dotfiles and filter_show or filter_hide
+      MiniFiles.refresh({ content = { filter = new_filter } })
+    end
+
+    -- Actually bind keymaps to toggle hidden, split open, etc.
     vim.api.nvim_create_autocmd('User', {
       pattern = 'MiniFilesBufferCreate',
       callback = function(args)
         local buf_id = args.data.buf_id
+        vim.keymap.set( 'n', '.', toggle_dotfiles,
+          { buffer = buf_id, desc = 'Toggle hidden files' })
         map_split(buf_id, '<C-s>', 'horizontal')
         map_split(buf_id, '<C-v>', 'vertical')
       end,
