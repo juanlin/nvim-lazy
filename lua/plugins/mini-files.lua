459ac599ee5e4568ca19b14edec3b0a6a7df9707
Juan Lin <9691057+juanlin@users.noreply.github.com>
2026-02-28 20:55:06 +0800

    feat(minif): remove toggle, use simple ignore list

--- lua/plugins/mini-files.lua
+++ lua/plugins/mini-files.lua
@@ -19,6 +19,12 @@ return {
       preview = true,
       width_preview = 50,
     },
+    content = {
+      filter = function(fs_entry)
+        local ignorelist = { '.git', '.DS_Store' }
+        return not vim.tbl_contains(ignorelist, fs_entry.name)
+      end
+    },
   },
   keys = {
     {
@@ -59,27 +65,11 @@ return {
       vim.keymap.set('n', lhs, rhs, { buffer = buf_id, desc = desc })
     end
 
-    -- Create toggle to show/hide hidden files
-    local show_dotfiles = true
-    local filter_show = function(fs_entry)
-      return true
-    end
-    local filter_hide = function(fs_entry)
-      return not vim.startswith(fs_entry.name, '.')
-    end
-    local toggle_dotfiles = function()
-      show_dotfiles = not show_dotfiles
-      local new_filter = show_dotfiles and filter_show or filter_hide
-      MiniFiles.refresh({ content = { filter = new_filter } })
-    end
-
     -- Actually bind keymaps to toggle hidden, split open, etc.
     vim.api.nvim_create_autocmd('User', {
       pattern = 'MiniFilesBufferCreate',
       callback = function(args)
         local buf_id = args.data.buf_id
-        vim.keymap.set( 'n', '.', toggle_dotfiles,
-          { buffer = buf_id, desc = 'Toggle hidden files' })
         map_split(buf_id, '<C-s>', 'horizontal')
         map_split(buf_id, '<C-v>', 'vertical')
       end,
