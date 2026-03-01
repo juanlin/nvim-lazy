a2233755354880a79263be4c1636d815746c6e51
Juan Lin <9691057+juanlin@users.noreply.github.com>
2026-03-02 07:34:01 +0800

    feat(nvim-treesitter): enable highlight and indent

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,11 +6,22 @@ return {
   config = function()
     local ts = require('nvim-treesitter')
 
-    local ensure_installed = {
+    local languages = {
       'lua', 'rust', 'python', 'go',
-      'typescript', 'tsx', 'javascript',
-      'regex',
+      'typescript', 'tsx', 'javascript', 'css',
+      'regex', 'markdown',
     }
-    ts.install(ensure_installed)
+    ts.install(languages)
+
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = languages,
+      callback = function()
+        -- Syntax highlighting, provided by Neovim
+        vim.treesitter.start()
+        -- Indentation, provided by nvim-treesitter
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
+
   end
 }
