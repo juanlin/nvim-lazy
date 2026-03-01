f5bf709921306e88c5c7fe2082fb6fa0345d5441
Juan Lin <9691057+juanlin@users.noreply.github.com>
2026-03-02 02:17:18 +0800

    feat(nvim-treesitter): new config for main branch

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,15 +3,14 @@ return {
   lazy = false,
   build = ':TSUpdate',
   branch = 'main',
-  event = { 'BufReadPost', 'BufNewFile', 'VeryLazy' },
-  opts = {
-    ensure_installed = {
+  config = function()
+    local ts = require('nvim-treesitter')
+
+    local ensure_installed = {
       'lua', 'rust', 'python', 'go',
       'typescript', 'tsx', 'javascript',
-    },
-    auto_install = true,
-    highlight = { enable = true },
-    indent = { enable = true },
-    folds = { enable = true },
-  },
+      'regex',
+    }
+    ts.install(ensure_installed)
+  end
 }
