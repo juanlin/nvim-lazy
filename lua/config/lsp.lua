bbae75026caecda1c550564ac0a9529d0ff806dd
Juan Lin <9691057+juanlin@users.noreply.github.com>
2026-03-02 11:55:05 +0800

    feat(lsp): disable semantic tokens (highlighting)

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -9,6 +9,11 @@ vim.api.nvim_create_autocmd('LspAttach', {
   group = lsp_setup,
   callback = function(ev)
     local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    -- Disable LSP semantic tokens, let nvim-treesitter handle coloring
+    if client and client.server_capabilities.semanticTokensProvider then
+      client.server_capabilities.semanticTokensProvider = nil
+    end
+    -- Enable autocompletion
     if client:supports_method('textDocument/completion') then
       vim.opt.completeopt = { 'menu', 'menuone', 'noinsert', 'fuzzy', 'popup' }
       vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
