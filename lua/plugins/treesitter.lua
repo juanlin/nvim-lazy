87270c550f1e6d29d62e0109622821f6dc4101d7
Juan Lin <9691057+juanlin@users.noreply.github.com>
2026-03-02 21:35:34 +0800

    feat(nvim-treesitter): enable folds

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -13,17 +13,31 @@ return {
     }
     ts.install(languages)
 
+    local ts_settings = vim.api.nvim_create_augroup('TSSettings', { clear = true })
     vim.api.nvim_create_autocmd('FileType', {
       desc = 'Set up nvim-treesitter highlighting and indentation',
-      group = vim.api.nvim_create_augroup('nvim-ts', { clear = true }),
+      group = ts_settings,
       pattern = languages,
       callback = function()
         -- Syntax highlighting, provided by Neovim
         vim.treesitter.start()
         -- Indentation, provided by nvim-treesitter
         vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        -- Folds using treesitter expr, provided by Neovim
+        vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+        vim.wo[0][0].foldmethod = 'expr'
       end,
     })
 
+    vim.api.nvim_create_autocmd({ 'BufReadPost', 'BufWinEnter' }, {
+      desc = 'Open all folds upon opening file',
+      group = ts_settings,
+      pattern = '*',
+      callback = function()
+        vim.defer_fn(function()
+          vim.cmd('normal! zR')
+        end, 0) -- wait for TS to load
+      end,
+    })
   end
 }
