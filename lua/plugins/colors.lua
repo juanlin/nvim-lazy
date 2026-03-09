320f26a311b1f3b56645366587fe4db04d25170a
Juan Lin <9691057+juanlin@users.noreply.github.com>
2026-03-09 22:40:41 +0800

    Revert "feat(colors): give indentscope a ridiculous color"
    This reverts commit 73c17d3c3d4999e6eb91c06a4c94408556840b03.

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -6,11 +6,6 @@ return {
     flavour = 'macchiato',  -- latte, frappe, macchiato, mocha
     transparent_background = true,
     -- auto_integrations = true,
-    integrations = {
-      mini = {
-        indentscope_color = 'yellow',
-      },
-    },
   },
   config = function(_, opts)
     require('catppuccin').setup(opts)
