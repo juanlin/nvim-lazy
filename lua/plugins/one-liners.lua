d921d44905b019f6b6296794487b4942c7efa85f
Juan Lin <9691057+juanlin@users.noreply.github.com>
2026-04-03 17:52:45 +0800

    feat(neoscroll): add neoscroll plugin

--- lua/plugins/one-liners.lua
+++ lua/plugins/one-liners.lua
@@ -2,4 +2,9 @@ return {
   { -- Git plugin
     'tpope/vim-fugitive',
   },
+  { -- Smooth scrolling
+    'karb94/neoscroll.nvim',
+    opts = { duration_multiplier = 0.30, },
+    event = 'VeryLazy',
+  },
 }
